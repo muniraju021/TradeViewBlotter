485f44406587612c138a6b27cfbde2ff6ee8ce4d
muniraju021 <muniraju021@gmail.com>
2021-07-10 13:29:54 +0530

    Pending Changes

---
+++ TraderBlotter.Api/Controllers/NetPositionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraderBlotter.Api.Controllers
+{
+    [Route("api/v{v:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class NetPositionController : ControllerBase
+    {
+
+    }
+}
